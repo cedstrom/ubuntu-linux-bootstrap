fdda25b7e593251ee362460fbdec03b35f8e716f
cedstrom <cedstrom@users.noreply.github.com>
2023-11-17 16:09:34 -0800

    Removing stuff I don't need

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,7 +25,7 @@ apt-get install -y curl wget git xclip vim git-daemon-run \
   apt-transport-https ca-certificates gnupg-agent build-essential software-properties-common
 
 # Chrome setup
-apt install chromium-browser
+# apt install chromium-browser
 
 # Docker setup - https://docs.docker.com/install/linux/docker-ce/ubuntu/
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -35,30 +35,30 @@ apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Docker Compose - https://docs.docker.com/compose/install/#install-compose-on-linux-systems
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
 # ASP.net setup, see - https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
 
 # VS Code setup - https://code.visualstudio.com/docs/setup/linux
-snap install --classic code
+# snap install --classic code
 
 # Node setup - https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
-apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+# apt-get install -y nodejs
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
-apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+# apt-get install -y nodejs
 
 # build tools
 apt-get install -y gcc g++ make
 
 # Yarn setup - https://yarnpkg.com/lang/en/docs/install/#debian-stable
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-apt-get update -y
-apt install yarn -y
+# apt-get update -y
+# apt install yarn -y
 
 # Python 3 (ignoring version 2)
 apt install -y python3 python3-pip
@@ -69,21 +69,21 @@ apt install -y build-essential libssl-dev libffi-dev python3-dev
 apt install -y  python3-dev
 
 # Go
-VERSION=1.15
-OS=linux
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz -O /tmp/go$VERSION.$OS-$ARCH.tar.gz
-tar -C /usr/local -xzf /tmp/go$VERSION.$OS-$ARCH.tar.gz
+# VERSION=1.15
+# OS=linux
+# wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz -O /tmp/go$VERSION.$OS-$ARCH.tar.gz
+# tar -C /usr/local -xzf /tmp/go$VERSION.$OS-$ARCH.tar.gz
 
 # Using "~" in sudo context will get "/root" so wild guess the profile path:
-USERS_PROFILE_FILENAME=/home/${SUDO_USER}/.profile
-if grep -Fq "/usr/local/go/bin" $USERS_PROFILE_FILENAME
-then
-    echo "GO path found in $USERS_PROFILE_FILENAME"
-else
-    echo '
-export PATH=$PATH:/usr/local/go/bin
-' >> $USERS_PROFILE_FILENAME
-fi
+# USERS_PROFILE_FILENAME=/home/${SUDO_USER}/.profile
+# if grep -Fq "/usr/local/go/bin" $USERS_PROFILE_FILENAME
+# then
+#     echo "GO path found in $USERS_PROFILE_FILENAME"
+# else
+#     echo '
+# export PATH=$PATH:/usr/local/go/bin
+# ' >> $USERS_PROFILE_FILENAME
+# fi
 
 # adds the cuurent user who is sudo'ing to a docker group:
 groupadd docker
